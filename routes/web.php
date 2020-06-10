28dd3f59614a427ea9642d6a5960257920029a19
Damon <yank0362@qq.com>
2020-06-10 21:03:39 +0800

    基础页面

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return 'ok';
-});
+Route::get('/', 'StaticPagesController@home');
+Route::get('/help', 'StaticPagesController@help');
+Route::get('/about', 'StaticPagesController@about');
