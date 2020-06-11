17e28d6cbbe8e27f4761a736f9406cde033a8fce
Damon <yank0362@qq.com>
2020-06-11 21:12:10 +0800

    用户注册页面

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.default')
+
 @section('content')
   <div class="jumbotron">
     <h1>Hello Laravel</h1>
@@ -9,7 +10,7 @@
       一切，将从这里开始。
     </p>
     <p>
-      <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+      <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
     </p>
   </div>
 @stop
