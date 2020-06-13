9c9e8b93582ab9963f6060ade9fc62125a75fbc2
Damon <yank0362@qq.com>
2020-06-13 20:59:06 +0800

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,10 +58,13 @@ class User extends Authenticatable
         return $this->hasMany(Status::class);
     }
 
-     public function feed()
+    public function feed()
     {
-        return $this->statuses()
-                    ->orderBy('created_at', 'desc');
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids, $this->id);
+        return Status::whereIn('user_id', $user_ids)
+                              ->with('user')
+                              ->orderBy('created_at', 'desc');
     }
 
     public function followers()
